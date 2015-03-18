9b166206c1d06eefa146148285e29243c6cc600d
saiheiy <saiheiy@gmail.com>
2015-03-17 22:55:29 -0500

    Update Board.py

--- Board.py
+++ Board.py
@@ -6,7 +6,7 @@ class Board(object):
         self.winner = None
         self.empty_str = ' '
         self.board_vec = [self.empty_str]*9
-        self.winning_patterns = [(0, 1, 2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,5,6)]
+        self.winning_patterns = [(0, 1, 2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
         self.board_string_patt = '\n| %s | %s | %s |\n| %s | %s | %s |\n| %s | %s | %s |\n'
         self.possible_squares = range(9)
         return
